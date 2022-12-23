2e56c0fbd871b9e01868814e676267c069332716
Stanko Culaja <stanko.culaja@kolotree.com>
2022-12-23 16:00:30 +0100

    Finished k8s scripts and auth.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/{apiKey}", (string apiKey) =>
+{
+    if (apiKey != app.Configuration["ApiKey"])
+    {
+        return "Dje ces kraaalju";
+    }
+    
+    return "Hello World!";
+});
 
 app.Run();
